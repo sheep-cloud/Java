48a9a1f6014a1a78a6f4a5300f7f7d179e4f8438
colg <121529654@qq.com>
2018-05-04 00:34:52 +0800

    add > 许雪里 XXL-JOB 轻量级分布式任务调度框架

---
+++ colg/xxl-job/xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobInfoController.java
@@ -0,0 +1,144 @@
+package com.xxl.job.admin.controller;
+
+import com.xxl.job.admin.core.enums.ExecutorFailStrategyEnum;
+import com.xxl.job.admin.core.model.XxlJobGroup;
+import com.xxl.job.admin.core.model.XxlJobInfo;
+import com.xxl.job.admin.core.route.ExecutorRouteStrategyEnum;
+import com.xxl.job.admin.dao.XxlJobGroupDao;
+import com.xxl.job.admin.service.XxlJobService;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.enums.ExecutorBlockStrategyEnum;
+import com.xxl.job.core.glue.GlueTypeEnum;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * index controller
+ * @author xuxueli 2015-12-19 16:13:16
+ */
+@Controller
+@RequestMapping("/jobinfo")
+public class JobInfoController {
+
+	@Resource
+	private XxlJobGroupDao xxlJobGroupDao;
+	@Resource
+	private XxlJobService xxlJobService;
+	
+	@RequestMapping
+	public String index(Model model, @RequestParam(required = false, defaultValue = "-1") int jobGroup) {
+
+		// 枚举-字典
+		model.addAttribute("ExecutorRouteStrategyEnum", ExecutorRouteStrategyEnum.values());	// 路由策略-列表
+		model.addAttribute("GlueTypeEnum", GlueTypeEnum.values());								// Glue类型-字典
+		model.addAttribute("ExecutorBlockStrategyEnum", ExecutorBlockStrategyEnum.values());	// 阻塞处理策略-字典
+		model.addAttribute("ExecutorFailStrategyEnum", ExecutorFailStrategyEnum.values());		// 失败处理策略-字典
+
+		// 任务组
+		List<XxlJobGroup> jobGroupList =  xxlJobGroupDao.findAll();
+		model.addAttribute("JobGroupList", jobGroupList);
+		model.addAttribute("jobGroup", jobGroup);
+
+		return "jobinfo/jobinfo.index";
+	}
+	
+	/**
+	 * 任务管理列表
+	 *
+	 * @param start
+	 * @param length
+	 * @param jobGroup
+	 * @param jobDesc
+	 * @param executorHandler
+	 * @param filterTime
+	 * @return
+	 */
+	@RequestMapping("/pageList")
+	@ResponseBody
+	public Map<String, Object> pageList(@RequestParam(required = false, defaultValue = "0") int start,  
+			@RequestParam(required = false, defaultValue = "10") int length,
+			int jobGroup, String jobDesc, String executorHandler, String filterTime) {
+		
+		return xxlJobService.pageList(start, length, jobGroup, jobDesc, executorHandler, filterTime);
+	}
+	
+	/**
+	 * 添加定时任务
+	 *
+	 * @param jobInfo
+	 * @return
+	 */
+	@RequestMapping("/add")
+	@ResponseBody
+	public ReturnT<String> add(XxlJobInfo jobInfo) {
+		return xxlJobService.add(jobInfo);
+	}
+	
+	/**
+	 * 编辑定时任务
+	 *
+	 * @param jobInfo
+	 * @return
+	 */
+	@RequestMapping("/update")
+	@ResponseBody
+	public ReturnT<String> update(XxlJobInfo jobInfo) {
+		return xxlJobService.update(jobInfo);
+	}
+	
+	/**
+	 * 删除定时任务
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/remove")
+	@ResponseBody
+	public ReturnT<String> remove(int id) {
+		return xxlJobService.remove(id);
+	}
+	
+	/**
+	 * 暂停定时任务
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/pause")
+	@ResponseBody
+	public ReturnT<String> pause(int id) {
+		return xxlJobService.pause(id);
+	}
+	
+	/**
+	 * 修改定时任务
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/resume")
+	@ResponseBody
+	public ReturnT<String> resume(int id) {
+		return xxlJobService.resume(id);
+	}
+	
+	/**
+	 * 触发定时任务
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/trigger")
+	@ResponseBody
+	public ReturnT<String> triggerJob(int id) {
+		return xxlJobService.triggerJob(id);
+	}
+	
+}
