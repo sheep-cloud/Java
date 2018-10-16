16fd5b0ec383730f883d378608a931f61688dd55
colg <121529654@qq.com>
2018-10-16 10:48:09 +0800

    update -> colg-dependencies

---
+++ colg/colg-dependencies/src/test/java/cn/colg/core/util/CoreTest.java
@@ -0,0 +1,14 @@
+package cn.colg.core.util;
+
+import org.junit.Test;
+
+/**
+ * 基础测试
+ *
+ * @author colg
+ */
+public class CoreTest {
+
+    @Test
+    public void testName() throws Exception {}
+}
