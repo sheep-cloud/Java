d8f8f7b50025fa4e47b09c834afbc88f9a8e8f8f
colg <121529654@qq.com>
2018-11-01 11:51:23 +0800

    update -> colg-dependencies

--- colg/colg-dependencies/src/main/java/cn/colg/util/ResultVoUtil.java
+++ colg/colg-dependencies/src/main/java/cn/colg/util/ResultVoUtil.java
@@ -1,5 +1,8 @@
 package cn.colg.util;
 
+import java.util.Collection;
+import java.util.List;
+
 import cn.colg.vo.ResultVo;
 
 /**
@@ -8,7 +11,7 @@ import cn.colg.vo.ResultVo;
  * @author colg
  */
 public class ResultVoUtil {
-    
+
     /**
      * 成功时默认响应
      *
@@ -18,7 +21,18 @@ public class ResultVoUtil {
     public static ResultVo success() {
         return new ResultVo(ResultVo.SUCCESS, ResultVo.MSG_SUCCESS);
     }
-    
+
+    /**
+     * 成功时响应消息
+     *
+     * @param msg 消息
+     * @return {code: 0, msg: "msg"}
+     * @author colg
+     */
+    public static ResultVo success(String msg) {
+        return new ResultVo(ResultVo.SUCCESS, msg);
+    }
+
     /**
      * 成功时默认响应
      *
@@ -27,18 +41,37 @@ public class ResultVoUtil {
      * @author colg
      */
     public static ResultVo success(Object data) {
-        return success().setData(data);
+        ResultVo resultVo = success();
+        convertData(data, resultVo);
+        return resultVo;
     }
 
     /**
      * 失败响应
      *
      * @param code 返回码
-     * @param msg  提示信息
+     * @param msg 提示信息
      * @return {code: code, msg: msg}
      * @author colg
      */
     public static ResultVo fail(Integer code, String msg) {
         return new ResultVo(code, msg);
+    }
+
+    /**
+     * 转换分页信息
+     *
+     * @param data 数据
+     * @param resultVo 响应对象
+     * @author colg
+     */
+    private static void convertData(Object data, ResultVo resultVo) {
+        if (data instanceof Collection<?>) {
+            List<?> list = (List<?>)data;
+            resultVo.setData(list)
+                    .setTotal((long)list.size());
+        } else {
+            resultVo.setData(data);
+        }
     }
 }
